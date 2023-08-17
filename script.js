c44bbb08b85c3f94f135b35c03183aae9b6a4811
Nishant Godse <125818822+Nishant5397@users.noreply.github.com>
2023-08-17 12:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-//your code here
 
+// Function to remove articles and sort lexicographically
+let bandNames - document.getElementById('band');
+function sortBandNames(bandNames) {
+  const articles = ['the', 'a', 'an'];
+
+  // Custom sorting function
+  function customSort(a, b) {
+    const cleanA = a.replace(/^the |^a |^an /i, ''); // Remove article from the beginning
+    const cleanB = b.replace(/^the |^a |^an /i, ''); // Remove article from the beginning
+    return cleanA.localeCompare(cleanB);
+  }
+
+  return bandNames.sort(customSort);
+}
+
+// Sort the band names
+const sortedBandNames = sortBandNames(bandNames);
+
+// Get the ul element to append the list items
+const ul = document.getElementById('band');
+
+// Add sorted band names to the ul element
+sortedBandNames.forEach(bandName => {
+  const li = document.createElement('li');
+  li.textContent = bandName;
+  ul.appendChild(li);
+});
